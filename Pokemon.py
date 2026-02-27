04caa2a3254ca5ae2dc52798869b3eba40ca8d7b
CooperKane <23010@wc.school.nz>
2026-02-27 14:18:38 +1300

    asdjasdhajda

--- Pokemon.py
+++ Pokemon.py
@@ -1,7 +1,8 @@
 import random
+import time
 
 wild_pokemon = [
-    {"Name" : "Charizard", "Type" : "Fire", "Level" : random.randint(1, 3), "Health" : random.randint(15, 25)},
+    {"Name" : "Charizard", "Type" : "Fire", "Level" : random.randint(1, 3), "Health" : random.randint(15, 25), "Attack" : ["Ember", random.randrange(3, 6), "Flamethrower", random.randrange(8, 11)]},
     {"Name" : "Venasaur", "Type" : "Grass", "Level" : random.randint(1, 3), "Health" : random.randint(15, 25)},
     {"Name" : "Blastoise", "Type" : "Water", "Level" : random.randint(1, 3), "Health" : random.randint(15, 25)},
     {"Name" : "Pikachu", "Type" : "Electric", "Level" : random.randint(1, 3), "Health" : random.randint(15, 25)},
@@ -9,12 +10,18 @@ wild_pokemon = [
     {"Name" : "Magikarp", "Type" : "Water", "Level" : random.randint(1, 3), "Health" : random.randint(15, 25)}
 ]
 
+def overworld_timer():
+    timer = random.randint(1, 5)
+    time.sleep(timer)
+    print("Battle begins")
+    battle()
+
 def battle():
-    x = random.randint(0, 5)
+    x = random.randint(0, len(wild_pokemon) - 1)
     pokemon = wild_pokemon[x]
     print("A wild", pokemon["Name"], "appeared")
     print("It's a", pokemon["Type"], "type Pokemon")
     print("It's level", pokemon["Level"])
     print("It has", pokemon["Health"], "health")
 
-battle()
+overworld_timer()

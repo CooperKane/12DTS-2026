916ff0057e772d71cda9657969883a77a4d1ba44
CooperKane <23010@wc.school.nz>
2026-03-10 12:27:17 +1300

    asdjasdhajda

---
+++ Final Assessment.py
@@ -0,0 +1,13 @@
+import random
+
+inventory = []
+
+def starting_sequence():
+    print("Welcome to Te Papa Escape Room")
+    print("You have to go through 5 different rooms to escape")
+    print("Each room will have its own theme")
+
+def first_room():
+
+
+starting_sequence()
